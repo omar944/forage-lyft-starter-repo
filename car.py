fd8538a6f774ed8d59b94426caf3816c25755524
omar <omaralbitar77@gmail.com>
2023-02-07 01:59:23 +0300

    add tires

--- car.py
+++ car.py
@@ -1,9 +1,11 @@
 from serviceable import Serviceable
 
 class Car(Serviceable):
-    def __init__(self, battery, engine):
+    def __init__(self, battery, engine, tire):
         self.battery = battery
         self.engine = engine
+        self.tire = tire
 
     def needs_service(self):
-        return self.engine.needs_service() or self.battery.needs_service()
+        return self.engine.needs_service() or self.battery.needs_service() or self.tire.needs_service()
+
--- test/test_battery.py
+++ test/test_battery.py
@@ -7,7 +7,7 @@ class TestSpindlerBattery(unittest.TestCase):
     
     def test_battery_should_be_serviced(self):
         current_date = datetime.today().date()
-        last_service_date = current_date.replace(year=current_date.year - 3)
+        last_service_date = current_date.replace(year=current_date.year - 4)
         battery = SpindlerBattery(current_date, last_service_date)
         self.assertTrue(battery.needs_service())
     
